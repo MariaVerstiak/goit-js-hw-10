dfd2e6793f157f29728a8abaeb4e3c0552bf948c
Maria <pishemuha2013@gmail.com>
2025-11-20 15:27:09 +0200

    correct mistakes

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,39 +1,65 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector(".form");
-
-form.addEventListener("submit", event => {
-  event.preventDefault();
-
-  const delay = Number(form.elements.delay.value);
-  const state = form.elements.state.value;
-
-  createPromise(delay, state)
-    .then(delay => {
-      iziToast.success({
-        title: "Success",
-        message: `✅ Fulfilled promise in ${delay}ms`,
-        position: "topRight",
-      });
-    })
-    .catch(delay => {
-      iziToast.error({
-        title: "Error",
-        message: `❌ Rejected promise in ${delay}ms`,
-        position: "topRight",
-      });
-    });
-});
-
-function createPromise(delay, state) {
+const makePromise = ({ delay, shouldResolve }) => {
+  console.log(delay);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (state === "fulfilled") {
+      if (shouldResolve) {
         resolve(delay);
       } else {
         reject(delay);
       }
     }, delay);
   });
+};
+
+function executePromise(promiseCallback) {
+  promiseCallback
+    .then((value) => {
+        iziToast.success({
+          message: `Fulfilled promise in ${value}ms`,
+        });
+    })
+    .catch((error) => {
+        iziToast.error({
+          message: `Rejected promise in ${error}ms`,
+        });
+    });
+   return;
 }
+
+function getValue(groupName) {
+  const radioButtons = document.getElementsByName(groupName);
+  for (const radioButton of radioButtons) {
+    if (radioButton.checked) {
+      return radioButton.value;
+    }
+  }
+  return null;
+}
+
+const SubmitBtn = document.querySelector('.submit-btn');
+const inputBox = document.querySelector('.input');
+const form = document.querySelector('.form');
+
+let userDalay = 0;
+inputBox.addEventListener('input', e => {
+  userDalay = e.target.value;
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  form.reset();
+});
+
+SubmitBtn.addEventListener('click', () => {
+  const selected = getValue('state');
+  if (selected === 'fulfilled') {
+    executePromise(makePromise({ delay: userDalay, shouldResolve: true }));
+  } else if (selected === 'rejected') {
+    executePromise(makePromise({ delay: userDalay, shouldResolve: false }));
+  } else {
+    return;
+  }
+});
